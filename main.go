591581bcacc9e52fa740a253bbfc6753f6caf522
NTetora <38845423+NTetora@users.noreply.github.com>
2024-07-30 02:20:36 +0900

    go sum追加

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello, World!")
+		c.String(http.StatusOK, "Hello, World!!")
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
